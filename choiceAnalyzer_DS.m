b2ea53b6c3560aaae927bb00a785be7958ab3f21
singerlab <singerlab@BMEWASINGLB01D.ad.gatech.edu>
2026-06-16 15:34:38 -0400

    Changes to choiceAnalyzer

--- choiceAnalyzer_DS.m
+++ choiceAnalyzer_DS.m
@@ -1,1 +1,11 @@
 function [output 1, output2] = choiceAnalyzer_DS(input1, input2)
+% Created by DS on 06/16/26
+% Python adaptation of Stephanie Prince's choice_analyzer python code 
+
+% creating the class
+classdef ChoiceAnalyzer(imported item that needs to be added)
+    properties
+        velocity_only
+        mask_value 
+        params(% dict then used: MATLAB equivalent needed)
+        grid_search_params(% dict then used: MATLAB equivalent needed)
